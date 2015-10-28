72491a1ee94cf7e99c98a903f5ba81df7701ddfd
jason <jason@zzq.org>
2015-10-28 01:39:16 -0600

    some lower level ground work

---
+++ eventmq.py
@@ -0,0 +1,136 @@
+"""
+eventmq
+"""
+import logging
+import zmq
+Context = zmq.Context
+
+LOG_LEVEL = logging.DEBUG
+PROTOCOLS = ('tcp', 'udp', 'pgm', 'epgm', 'inproc', 'ipc')
+VALID_TOPIC_TYPES = (int, str)
+
+
+class STATUS(object):
+    ready = 0
+    started = 2
+    stopping = 3
+
+
+class LoggerMixin(object):
+    """
+    Provides self.logger
+    """
+    FORMAT_STANDARD = logging.Formatter('%(asctime)s - %(name)s - '
+                                        '%(levelname)s - %(message)s')
+
+    def __init__(self, *args, **kwargs):
+        super(LoggerMixin, self).__init__(*args, **kwargs)
+        self._stream_handler = logging.StreamHandler()
+        self._logger = None
+        self._stream_handler.setFormatter(self.FORMAT_STANDARD)
+
+    @property
+    def logger(self):
+        if not self._logger:
+            self._logger = logging.getLogger(self.__class__.__name__.lower())
+            self._logger.setLevel(LOG_LEVEL)
+            self._logger.addHandler(self._stream_handler)
+        return self._logger
+
+
+class Publisher(LoggerMixin):
+    """
+    Handles sockets for publishing messages
+
+    Attrs:
+        context (:class:`zmq.Context`)
+        socket_type
+        socket (:class:`zmq.Socket`): raw zmq socket
+    """
+
+    def __init__(self, xpub=False, context=None):
+        """
+        """
+        super(Publisher, self).__init__()
+        self.context = context or zmq.Context.instance()
+        self.socket_type = zmq.XPUB if xpub else zmq.PUB
+        self.socket = self.context.socket(self.socket_type)
+        self.status = STATUS.ready
+
+    def listen(self, addr="tcp://127.0.0.1:47331"):
+        """
+        Starts listening on an address
+        """
+        self.socket.bind(addr)
+        self.status = STATUS.started
+
+    def send(self, msg, topic=''):
+        """
+        Send a message to all subscribers of topic.
+
+        Raises:
+            See :func:`validate_topic_type` for list of possible Exceptions.
+
+        """
+        self.logger.debug('Trying to publish to topic "%s" (type: %s): %s' %
+                          (topic, type(topic), msg))
+        validate_topic_type(topic)
+        self.socket.send_multipart([topic, msg])
+
+
+class Subscriber(LoggerMixin):
+    """
+    Handles sockets for subscribing to messages
+    """
+
+    def __init__(self, xsub=False, context=None):
+        """
+        """
+        super(Subscriber, self).__init__()
+        self.context = context or zmq.Context.instance()
+        self.socket_type = zmq.XSUB if xsub else zmq.SUB
+        self.socket = self.context.socket(self.socket_type)
+        self.status = STATUS.ready
+
+        self.subscriptions = ()
+
+    def connect(self, addr="tcp://127.0.0.1:47331"):
+        """
+        Connects to a publisher at `addr`
+        """
+        self.socket.connect(addr)
+        self.status = STATUS.started
+
+    def subscribe(self, topic):
+        """
+        """
+        validate_topic_type(topic)
+        self.socket.setsockopt(zmq.SUBSCRIBE, topic)
+
+    def unsubscribe(self, topic):
+        """
+        """
+        validate_topic_type(topic)
+        self.socket.setsockopt(zmq.UNSUBSCRIBE, topic)
+
+    def receive(self):
+        """
+        """
+        topic, msg = self.socket.recv_multipart()
+        return topic, msg
+
+
+def validate_topic_type(topic):
+    """
+    Validates the topic as the right type.
+
+    .. warning::
+       You are incharge of catching these exceptions and logging some
+       meaningful output.
+
+    Raises: `UnicodeError` `ValueError`
+    """
+    if isinstance(topic, unicode):
+        raise UnicodeError('Topics must not be unicode')
+    elif not isinstance(topic, VALID_TOPIC_TYPES):
+        raise ValueError('Invalid Topic Type %s' % type(topic))
---
+++ tests.py
@@ -0,0 +1,143 @@
+import logging
+import unittest
+
+import zmq
+import eventmq
+
+INPROC = "inproc://derp"
+
+
+class PublisherTestCase(unittest.TestCase):
+    def setUp(self):
+        self._zcontext = zmq.Context.instance()
+        self._zmq_subscriber = None
+        self._publisher = eventmq.Publisher()
+        self._publisher.logger.setLevel(logging.WARNING)
+
+    @property
+    def publisher(self):
+        """
+        property for `self._publisher` so testing `Publisher.__init__` doesn't
+        clog the INPROC tube.
+        """
+        if self._publisher.status == eventmq.STATUS.ready:
+            self._publisher.listen(INPROC)
+        return self._publisher
+
+    @property
+    def zsubscriber(self):
+        """
+        Your very own zmq subscriber socket
+        """
+        if not self._zmq_subscriber:
+            self._zmq_subscriber = self._zcontext.socket(zmq.SUB)
+            self._zmq_subscriber.connect(INPROC)
+
+        return self._zmq_subscriber
+
+    def test_default_pub_creation(self):
+        publisher = eventmq.Publisher()
+        self.assertEqual(publisher.socket.socket_type, zmq.PUB)
+
+    def test_xpub_creation(self):
+        publisher = eventmq.Publisher(xpub=True)
+        self.assertEqual(publisher.socket.socket_type, zmq.XPUB)
+
+    def test_send_unicode(self):
+        test_message = 'testmsg'
+
+        self.zsubscriber.setsockopt(zmq.SUBSCRIBE, '')
+        self.publisher.send(test_message)
+        topic, msg = self.zsubscriber.recv_multipart()
+
+        self.assertEqual(msg, test_message)
+
+    def test_valueerror_weird_topic(self):
+        self.assertRaises(ValueError, self.publisher.send, 'asdf',
+                          topic=3924.34)
+
+    def test_unicodeerror_topic(self):
+        self.assertRaises(UnicodeError, self.publisher.send, 'asdf',
+                          topic=u'a')
+
+    def tearDown(self):
+        del self._publisher
+        del self._zmq_subscriber
+
+
+class SubscriberTestCase(unittest.TestCase):
+    TOPIC = 'derp topic'
+    uTOPIC = u'derp topic'
+    MSG = 'batton down the hatchs'
+    uMSG = u'swab the poop deck'
+
+    def setUp(self):
+        self._zcontext = zmq.Context.instance()
+        self._zmq_publisher = None
+        self._subscriber = eventmq.Subscriber()
+        self._subscriber.logger.setLevel(logging.WARNING)
+
+    @property
+    def subscriber(self):
+        """
+        property for `self._subscriber` so testing `Subscriber`.__init__
+        doesn't clog the INPROC tubes.
+
+        Returns: :class:`Subscriber`
+        """
+        if self._subscriber.status == eventmq.STATUS.ready:
+            self._subscriber.connect(INPROC)
+        return self._subscriber
+
+    @property
+    def zpublisher(self):
+        """
+        Your very own zmq publisher socket
+        """
+        if not self._zmq_publisher:
+            self._zmq_publisher = self._zcontext.socket(zmq.PUB)
+            self._zmq_publisher.bind(INPROC)
+        return self._zmq_publisher
+
+    def test_default_sub_creation(self):
+        subscriber = eventmq.Subscriber()
+        self.assertEqual(subscriber.socket.socket_type, zmq.SUB)
+
+    def test_xsub_creation(self):
+        subscriber = eventmq.Subscriber(xsub=True)
+        self.assertEqual(subscriber.socket.socket_type, zmq.XSUB)
+
+    def test_subscribe(self):
+        self.subscriber.subscribe(self.TOPIC)
+
+        self.zpublisher.send_multipart([self.TOPIC, self.MSG])
+
+        topic, msg = self.subscriber.receive()
+
+        self.assertEqual(topic, self.TOPIC)
+        self.assertEqual(msg, self.MSG)
+
+    def test_unsubscribe(self):
+        self.subscriber.socket.setsockopt(zmq.SUBSCRIBE, self.TOPIC)
+
+        self.subscriber.unsubscribe(self.TOPIC)
+        self.zpublisher.send_multipart([self.TOPIC, self.MSG])
+
+        self.assertEqual(self.subscriber.socket.poll(100), 0)
+        
+
+    def test_unicodeerror_unicode_topic(self):
+        self.assertRaises(UnicodeError, self.subscriber.subscribe,
+                          self.uTOPIC)
+
+    def test_valueerror_non_str_int_topic(self):
+        self.assertRaises(ValueError, self.subscriber.subscribe, 1.39)
+        self.assertRaises(ValueError, self.subscriber.subscribe, list('v'))
+
+    def tearDown(self):
+        del self._zmq_publisher
+        del self._subscriber
+
+
+if __name__ == "__main__":
+    unittest.main()
