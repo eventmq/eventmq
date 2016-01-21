ab0bad286b47fb36a59a3859f66367929d746c26
sideshowdave7 <david@eventboard.io>
2016-01-20 21:16:21 -0700

    Bump setup.py version to 0.1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='eventmq',
-    version='0.1.0',
+    version='0.1.3',
     description='EventMQ messaging system based off ZeroMQ',
     packages=find_packages(),
     install_requires=['pyzmq>=14.6.0',
