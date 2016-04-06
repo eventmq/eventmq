10c194ea7013123b40b8a8a3f52031df42724bd0
sideshowdave7 <david@eventboard.io>
2016-04-05 18:14:04 -0600

    update version to 0.1.9

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='eventmq',
-    version='0.1.3',
+    version='0.1.9',
     description='EventMQ messaging system based off ZeroMQ',
     packages=find_packages(),
     install_requires=['pyzmq==14.6.0',
