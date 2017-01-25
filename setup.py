0a66990263b1c62c28481fb0fe84388a5778ded5
jason <jason@zzq.org>
2017-01-25 11:25:42 -0700

    Bump version to 0.3-rc9

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='eventmq',
-    version='0.3-rc8',
+    version='0.3-rc9',
     description='EventMQ messaging system based on ZeroMQ',
     packages=find_packages(),
     install_requires=['pyzmq==15.4.0',
